5b465f796ac4e553459e0a2e0737d5cea0db4913
JoshuaSMoore <J_Moore23@hotmail.com>
2021-10-06 16:09:07 -0600

    Update

--- Myst.client/src/env.js
+++ Myst.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'jmoore07.us.auth0.com'
+export const clientId = 'mm6F2SzDZrPMB9IipadiK2od6OXoO2NX'
+export const audience = 'https://studentauth.co'
