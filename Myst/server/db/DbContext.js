a67a2018b960b76fc2a7b9b843a3c5ebfc6f5864
Talan Weeks <Talan.jakoby.weeks@gmail.com>
2021-10-07 11:51:39 -0600

    db context finsihed

--- Myst/server/db/DbContext.js
+++ Myst/server/db/DbContext.js
@@ -2,12 +2,21 @@ import mongoose from 'mongoose'
 import { AccountSchema, ProfileSchema } from '../models/Account'
 import { ValueSchema } from '../models/Value'
 import { GameSchema } from '../models/Game'
+import { FollowSchema } from '../models/Follow'
+import { TrackedGameSchema } from '../models/TrackedGame'
+import { PostSchema } from '../models/Post'
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
   Profiles = mongoose.model('Profile', ProfileSchema, 'accounts');
   Game = mongoose.model('Game', GameSchema);
+
+  Follow = mongoose.model('Follow', FollowSchema);
+
+  TrackedGame = mongoose.model('TrackedGame', TrackedGameSchema);
+
+  Post = mongoose.model('Post', PostSchema);
 }
 
 export const dbContext = new DbContext()
