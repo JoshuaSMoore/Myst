b9030653ce9a45d75104a2b05ee1cd18dc50f293
Kyle Hanna <Hanna.Kyle@live.com>
2021-10-07 11:02:10 -0600

    updated game  for dbcontext

--- Myst/server/db/DbContext.js
+++ Myst/server/db/DbContext.js
@@ -1,11 +1,13 @@
 import mongoose from 'mongoose'
 import { AccountSchema, ProfileSchema } from '../models/Account'
 import { ValueSchema } from '../models/Value'
+import { GameSchema } from '../models/Game'
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
   Profiles = mongoose.model('Profile', ProfileSchema, 'accounts');
+  Games = mongoose.model('Game', GameSchema);
 }
 
 export const dbContext = new DbContext()
