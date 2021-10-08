5efe0d9ab2938212475af1dd9f68c46918b94b5b
NickSondrup <nicholas.sondrup@gmail.com>
2021-10-08 10:29:24 -0600

    profile edit in service

--- Myst.client/src/pages/ProfilePage.vue
+++ Myst.client/src/pages/ProfilePage.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="row m-5">
-    <h3> Profile Page</h3>
-    <h4>{{ profile.picture }}</h4>
-    <h4>{{ profile.name }}</h4>
+    <div class="col-12 d-flex justify-content-center">
+      <div class="profile-card card bg-dark text-light">
+        <h3> Profile Page</h3>
+        <img class="rounded" :src="profile.picture" alt="" height="65" width="65" />
+        <h4>{{ profile.name }}</h4>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -19,5 +23,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.profile-card{
+  width: 70rem;
+  height: 30rem;
+}
 </style>
