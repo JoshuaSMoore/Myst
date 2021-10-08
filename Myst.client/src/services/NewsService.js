0d828b2efa91c2249621bab49d6f0ceb45708e20
Derek Shain <derek0anthony@gmail.com>
2021-10-08 09:13:46 -0600

    update

--- Myst.client/src/services/NewsService.js
+++ Myst.client/src/services/NewsService.js
@@ -1,13 +1,7 @@
 import axios from 'axios'
 import { AppState } from '../AppState'
-import { News } from '../models/NewsCard'
 import { logger } from '../utils/Logger'
 
-// const apiKey = '1cbf381c85msh2ea0ee7bd78a7e6p1bbb8fjsn5db7d3da7e8b'
-// const newsApi = axios.create({
-//   baseURL: 'https://mmo-games.p.rapidapi.com/latestnews'
-// })
-// const axios = require('axios').default
 const options = {
   method: 'GET',
   url: 'https://mmo-games.p.rapidapi.com/latestnews',
@@ -23,7 +17,6 @@ class NewsService {
       AppState.currentPage = res.data.page
       AppState.totalPages = res.data.total_pages
       AppState.news = res.data
-      // AppState.news = res.data.results.map(n => new News(n))
     })
   }
 }
