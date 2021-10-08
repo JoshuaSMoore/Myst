d984f2d2acb689d07d87effa0ed0053ce32e8bac
Kyle Hanna <Hanna.Kyle@live.com>
2021-10-08 10:37:45 -0600

    updating profile stuff

--- Myst.client/src/pages/ProfilePage.vue
+++ Myst.client/src/pages/ProfilePage.vue
@@ -2,12 +2,22 @@
   <div class="row m-5">
     <div class="col-12 d-flex justify-content-center">
       <div class="profile-card card bg-dark text-light">
-        <h3> Profile Page</h3>
+        <h3> Profile Page</h3>     <i class="mdi mdi-cog mdi-48px text-white elevation-5 " type="button" data-bs-toggle="modal" data-bs-target="#profile-form">
+        </i>
+
         <img class="rounded" :src="profile.picture" alt="" height="65" width="65" />
         <h4>{{ profile.name }}</h4>
       </div>
     </div>
   </div>
+  <Modal id="profile-form">
+    <template #modal-title>
+      <h4>Update Profile</h4>
+    </template>
+    <template #modal-body>
+      <ProfileForm />
+    </template>
+  </Modal>
 </template>
 
 <script>
