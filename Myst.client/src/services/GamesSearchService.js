d1063753a85b00c28869b1b8dc8cadd35b1fcdb6
Derek Shain <derek0anthony@gmail.com>
2021-10-11 09:01:17 -0600

    trying to add pagination

--- Myst.client/src/services/GamesSearchService.js
+++ Myst.client/src/services/GamesSearchService.js
@@ -30,12 +30,45 @@ class GamesSearchService {
     axios.request(searchGames).then(function(res) {
       logger.log(res.data.results)
       AppState.currentPage = res.data.page
-      AppState.totalPages = res.data.total_pages
+      AppState.next = res.data.next
+      AppState.previous = res.data.previous
       AppState.games = res.data.results.map(g => new Game(g))
     }).catch(function(error) {
       logger.error(error)
     })
     router.push({ name: 'Search' })
+  }
+
+  async getOld(query) {
+    AppState.currentPage--
+    AppState.games = []
+    AppState.page = {}
+    const oldGame = {
+      method: 'GET',
+      url: `https://api/rawg.io/api/games?key=004cc6f4ef734a4a8725e3082070efd6&page=${AppState.currentPage}&search=${query}`
+    }
+    axios.request(oldGame).then(function(res) {
+      AppState.page = res.data
+      AppState.games = res.data.results.map(g => new Game(g))
+    }).catch(function(error) {
+      logger.error(error)
+    })
+  }
+
+  async getNew(query) {
+    AppState.currentPage++
+    AppState.games = []
+    AppState.page = {}
+    const newGame = {
+      method: 'GET',
+      url: `https://api/rawg.io/api/games?key=004cc6f4ef734a4a8725e3082070efd6&page=${AppState.currentPage}&search=${query}`
+    }
+    axios.request(newGame).then(function(res) {
+      AppState.page = res.data
+      AppState.games = res.data.results.map(g => new Game(g))
+    }).catch(function(error) {
+      logger.error(error)
+    })
   }
 
   async getGameById(id) {
