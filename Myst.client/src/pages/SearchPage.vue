90cd7e6b6a2d7bd7b16b273c6d66581bbac2fda8
Derek Shain <derek0anthony@gmail.com>
2021-10-11 11:14:19 -0600

    GAME STUFF

--- Myst.client/src/pages/SearchPage.vue
+++ Myst.client/src/pages/SearchPage.vue
@@ -1,17 +1,21 @@
 <template>
-  <div class="row m-5 p-0">
+  <div class="row m-5 p-0 align-items-center d-flex justify-content-center">
+    <div class="text-muted text-dark text-center mt-3">
+      <h2> PAGE {{ currentPage }} </h2>
+    </div>
     <GameSearchCard v-for="g in games" :key="g.id" :game="g" class="m-2" />
     <div class="col-12 d-flex justify-content-around">
-      <button class="btn btn-secondary" @click="getOld()" v-if="page.new">
+      <button class="btn btn-secondary" @click="getOld()" v-if="previous">
         Previous
       </button>
-      <div v-if="!page.new"></div>
+      <div v-if="!previous"></div>
       <div class="text-muted text-dark">
-        {{ page.new }}
-      </div><div v-if="!page.old"></div>
-      <button class="btn btn-secondary" @click="getNew()" v-if="page.old">
+        <h2> PAGE {{ currentPage }} </h2>
+      </div>
+      <button class="btn btn-secondary" @click="getNew()" v-if="next">
         Next
       </button>
+      <div v-if="!next"></div>
     </div>
   </div>
 </template>
@@ -37,17 +41,19 @@ export default {
       query,
       games: computed(() => AppState.games),
       page: computed(() => AppState.page),
+      next: computed(() => AppState.next),
+      previous: computed(() => AppState.previous),
       currentPage: computed(() => AppState.currentPage),
       async getNew() {
         try {
-          await gamesSearchService.getNew()
+          await gamesSearchService.getNew(AppState.currentPage)
         } catch (error) {
           Pop.toast('error', error)
         }
       },
       async getOld() {
         try {
-          await gamesSearchService.getOld()
+          await gamesSearchService.getOld(AppState.currentPage)
         } catch (error) {
           Pop.toast('error', error)
         }
