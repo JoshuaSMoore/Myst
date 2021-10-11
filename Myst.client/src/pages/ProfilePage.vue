f8f3d101e91560c0d0fa94d53f97ca758b6b0b8a
NickSondrup <nicholas.sondrup@gmail.com>
2021-10-11 10:23:58 -0600

    profile changes

--- Myst.client/src/pages/ProfilePage.vue
+++ Myst.client/src/pages/ProfilePage.vue
@@ -1,36 +1,60 @@
 <template>
-  <div class="row m-5">
-    <div class="col-12 d-flex justify-content-center">
-      <div class="profile-card card bg-dark text-light">
-        <h3>
-          Profile Page <i class="mdi mdi-cog mdi-24px text-white elevation-5 " type="button" data-bs-toggle="modal" data-bs-target="#profile-form">
-          </i>
-        </h3>
-
-        <img class="rounded" :src="profile.picture" alt="" height="100" width="100" />
-        <h4>{{ profile.name }}</h4>
-        <p v-if="profile.bio">
-          <b>Bio:</b> {{ profile.bio }}
-        </p>
-        <p v-if="profile.twitch">
-          <b class="mdi mdi-twitch  mdi-24px"></b> {{ profile.twitch }}
-        </p>
-        <p v-if="profile.github">
-          <b class="mdi mdi-github mdi-24px"></b> {{ profile.github }}
-        </p>
-        <p v-if="profile.steam">
-          <b class="mdi mdi-steam mdi-24px"></b> {{ profile.steam }}
-        </p>
-        <p v-if="profile.xbox">
-          <b class="mdi mdi-microsoft-xbox mdi-24px"></b> {{ profile.xbox }}
-        </p>
-        <p v-if="profile.playstation">
-          <b class="mdi mdi-sony-playstation mdi-24px"></b> {{ profile.playstation }}
-        </p>
-
-        <p v-if="profile.nintendo">
-          <b class="mdi mdi-nintendo-switch mdi-24px"></b> {{ profile.nintendo }}
-        </p>
+  <div class="container-fluid">
+    <div class="row my-5">
+      <div class="col-12">
+        <h2>Welcome, {{ profile.name }}</h2>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12 d-flex justify-content-center">
+        <div class="profile-card container card bg-dark text-light">
+          <div class="row">
+            <div class="col-md-4 m-auto">
+              <div class="card live-card bg-light text-dark d-flex justify-content-center align-items-center">
+                <b>LIVE STREAM COMING SOON</b>
+              </div>
+            </div>
+            <div class="col-md-8">
+              <div class="d-flex m-4">
+                <div>
+                  <img class="rounded" :src="profile.picture" alt="" height="200" width="200" />
+                </div>
+                <div class="mx-4">
+                  <p v-if="profile.bio" class="my-1">
+                    <b>Bio:</b> {{ profile.bio }}
+                  </p>
+                  <p v-if="profile.twitch" class="my-1">
+                    <b class="mdi mdi-twitch  mdi-24px"></b> {{ profile.twitch }}
+                  </p>
+                  <p v-if="profile.github" class="my-1">
+                    <b class="mdi mdi-github mdi-24px"></b> {{ profile.github }}
+                  </p>
+                  <p v-if="profile.steam" class="my-1">
+                    <b class="mdi mdi-steam mdi-24px"></b> {{ profile.steam }}
+                  </p>
+                  <p v-if="profile.xbox" class="my-1">
+                    <b class="mdi mdi-microsoft-xbox mdi-24px"></b> {{ profile.xbox }}
+                  </p>
+                  <p v-if="profile.playstation" class="my-1">
+                    <b class="mdi mdi-sony-playstation mdi-24px"></b> {{ profile.playstation }}
+                  </p>
+                  <p v-if="profile.nintendo" class="my-1">
+                    <b class="mdi mdi-nintendo-switch mdi-24px"></b> {{ profile.nintendo }}
+                  </p>
+                </div>
+              </div>
+              <div class="card friend-card bg-light text-dark mx-5">
+                <b>Friends here</b>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12 d-flex justify-content-end">
+              <i class="mdi mdi-cog f-20 text-light" type="button" data-bs-toggle="modal" data-bs-target="#profile-form">
+              </i>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -58,7 +82,15 @@ export default {
 
 <style lang="scss" scoped>
 // .profile-card{
-//   width: 70rem;
+//   width: 90rem;
 //   height: 30rem;
 // }
+
+.live-card{
+  height: 15rem;
+}
+
+.friend-card{
+  height: 5rem;
+}
 </style>
