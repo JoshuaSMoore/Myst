830907f58f6920c620b554f71eb560e29c646643
NickSondrup <nicholas.sondrup@gmail.com>
2021-10-12 15:00:21 -0600

    followed games working

---
+++ Myst.client/src/components/FollowedGames.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="text-light">
+    {{ followedGame.name }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    followedGame: { type: Object, required: true }
+  },
+  setup() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
