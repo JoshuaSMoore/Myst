9caa39a8ca54273330b211a49b082c764a1d6a02
NickSondrup <nicholas.sondrup@gmail.com>
2021-10-12 14:46:41 -0600

    getting followed games worked

--- Myst.client/src/services/TrackedGamesService.js
+++ Myst.client/src/services/TrackedGamesService.js
@@ -2,6 +2,7 @@ import { AppState } from '../AppState.js'
 import { logger } from '../utils/Logger.js'
 import { api } from './AxiosService.js'
 import { gamesSearchService } from './GamesSearchService.js'
+import axios from 'axios'
 
 class TrackedGamesService {
   async createTrackedGame(gameId) {
@@ -12,11 +13,26 @@ class TrackedGamesService {
   }
 
   async getTrackedGames() {
+    AppState.followedGames = []
     const tracked = AppState.trackedGames
     tracked.forEach(async t => {
-      const game = await gamesSearchService.getGameById(t.gameId)
-      AppState.trackedGames.push(game)
-      logger.log('here trackedGames', AppState.trackedGames)
+      await this.getGameById(t.gameId)
+    })
+    logger.log('here trackedGames', AppState.followedGames)
+  }
+
+  async getGameById(id) {
+    AppState.game = {}
+    const game = {
+      method: 'GET',
+      url: `https://api.rawg.io/api/games/${id}?key=004cc6f4ef734a4a8725e3082070efd6`
+    }
+    axios.request(game).then(function(res) {
+      logger.log(res.data)
+      AppState.followedGames.push(res.data)
+      logger.log('after getGameById', AppState.followedGames)
+    }).catch(function(error) {
+      logger.error(error)
     })
   }
 }
