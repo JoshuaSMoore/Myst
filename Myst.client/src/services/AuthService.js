3259b50007beef9fd814c999998bcf037dd928e8
Talan Weeks <Talan.jakoby.weeks@gmail.com>
2021-10-13 17:07:28 -0600

    test

--- Myst.client/src/services/AuthService.js
+++ Myst.client/src/services/AuthService.js
@@ -29,7 +29,7 @@ AuthService.on(AuthService.AUTH_EVENTS.AUTHENTICATED, async function() {
   await accountService.getAccount()
   socketService.authenticate(AuthService.bearer)
   // NOTE if there is something you want to do once the user is authenticated, place that here
-  firebaseService.login()
+  await firebaseService.login()
   await accountService.getTrackedGames(AppState.account.id)
   await trackedGamesService.getTrackedGames()
 })
