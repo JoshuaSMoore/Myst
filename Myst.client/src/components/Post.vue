b0f90586c3808a55778688047580f8a9f81d9747
Derek Shain <derek0anthony@gmail.com>
2021-10-13 15:11:54 -0600

    test

--- Myst.client/src/components/Post.vue
+++ Myst.client/src/components/Post.vue
@@ -2,15 +2,23 @@
   <div class="post">
     <div class="card bg-info text-light shadow" style="width: 18rem">
       <div class="card-body container-fluid">
-        <img :src="post.mediaUrl" v-if="post.mediaUrl?.includes('Image')" width="300" class="ms-3 img-fluid" alt="">
+        <img :src="post.mediaUrl"
+             v-if="post.mediaUrl?.includes('Image')"
+             width="300"
+             class="img-fluid selectable"
+             alt=""
+        >
         <video class="img-fluid" controls v-else>
           <source :src="post.mediaUrl" type="video/mp4" />
           <source src="movie.ogg" type="video/ogg" />
         </video>
       </div>
-      <div class="d-flex card-footer bg-primary text-dark">
+      <div class="d-flex card-footer bg-primary text-dark selectable"
+           data-bs-toggle="modal"
+           :data-bs-target="'#p-modal-'+post.id"
+      >
         <h5>{{ post.body }}</h5>
-        <i class="mdi mdi-delete-forever selectable text-danger f-20 mx-3" title="Delete Post" v-if="post.creatorId === account.id" @click="deletePost(post.id)"></i>
+        <i class="mdi mdi-delete-forever selectable text-light f-20 mx-3" title="Delete Post" v-if="post.creatorId === account.id" @click="deletePost(post.id)"></i>
       </div>
     </div>
   </div>
---
+++ Myst.client/src/components/PostInfo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="bg-dark text-light">
+    <div class="form-group header">
+    </div>
+  </div>
+</template>
+
+<script>
+import { computed } from '@vue/runtime-core'
+import { News } from '../models/NewsCard'
+import { AppState } from '../AppState'
+import { logger } from '../utils/Logger'
+import { Post } from '../models/Post'
+export default {
+  props: {
+    post: {
+      type: Post,
+      required: true
+    }
+  },
+  setup(props) {
+    return {
+      posts: computed(() => AppState.posts)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
