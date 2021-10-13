23afd5296decff860703bb4339504f1dd36080f0
Derek Shain <derek0anthony@gmail.com>
2021-10-13 11:20:48 -0600

    styling

--- Myst.client/src/components/Post.vue
+++ Myst.client/src/components/Post.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="post">
-    <div class="card shadow p-0" style="width: 18rem">
+    <div class="card bg-info text-light shadow" style="width: 18rem">
       <div class="card-body container-fluid">
         <img :src="post.mediaUrl" v-if="post.mediaUrl?.includes('Image')" width="300" class="ms-3 img-fluid" alt="">
         <video class="img-fluid" controls v-else>
@@ -8,7 +8,7 @@
           <source src="movie.ogg" type="video/ogg" />
         </video>
       </div>
-      <div class="card-footer text-dark">
+      <div class="d-flex card-footer bg-primary text-dark">
         <h5>{{ post.body }}</h5>
         <i class="mdi mdi-delete-forever selectable text-danger f-20 mx-3" title="Delete Post" v-if="post.creatorId === account.id" @click="deletePost(post.id)"></i>
       </div>
