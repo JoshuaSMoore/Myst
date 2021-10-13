3e78c80fd5ae105ba00a3cb945d7d75306588809
Derek Shain <derek0anthony@gmail.com>
2021-10-13 09:59:39 -0600

    NEW PUSH firebase

--- Myst.client/src/env.js
+++ Myst.client/src/env.js
@@ -4,3 +4,13 @@ export const useSockets = false
 export const domain = 'jmoore07.us.auth0.com'
 export const clientId = 'mm6F2SzDZrPMB9IipadiK2od6OXoO2NX'
 export const audience = 'https://studentauth.co'
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBzDndRYYFiLu619zZVcPRX4nFn4LAqg18',
+  authDomain: 'myst-547a1.firebaseapp.com',
+  projectId: 'myst-547a1',
+  storageBucket: 'myst-547a1.appspot.com',
+  messagingSenderId: '341128621647',
+  appId: '1:341128621647:web:d7e47d252813c63f32443a',
+  measurementId: 'G-Z74ES3NCH1'
+}
---
+++ Myst.client/src/services/FirebaseService.js
@@ -0,0 +1,31 @@
+import { AppState } from '../AppState'
+import { fbAuth, storage } from '../utils/FirebaseProvider'
+import { logger } from '../utils/Logger'
+import { api } from './AxiosService'
+
+class FirebaseService {
+  async upload(data, type) {
+    const collection = storage.ref(type)
+    const resource = collection.child(data.name)
+    // const resource = collection.child(AppState.account.id).child(data.name)
+    const snapshot = await resource.put(data, {
+      customMetadata: {
+        uid: AppState.account.id, size: data.size, type: data.type
+      }
+    })
+    const url = await snapshot.ref.getDownloadURL()
+    return url
+  }
+
+  async login() {
+    try {
+      const res = await api.get('/account/firebase-token')
+      const firebaseAuthToken = res.data.token
+      await fbAuth.signInWithCustomToken(firebaseAuthToken)
+    } catch (e) {
+      logger.error('[FIREBASE_AUTH] unable to authenticate', e)
+    }
+  }
+}
+
+export const firebaseService = new FirebaseService()
