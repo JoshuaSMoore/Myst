f66877006259f2be5b5e235931c4f54a15751dcd
NickSondrup <nicholas.sondrup@gmail.com>
2021-10-14 14:06:28 -0600

    follow/unfollow working with bugs

--- Myst.client/src/services/FollowService.js
+++ Myst.client/src/services/FollowService.js
@@ -14,6 +14,14 @@ class FollowService {
     AppState.following = res.data
     logger.log('JKDLFLKS:DJJFLJ:DSLKF:SD', AppState.following)
   }
+
+  async deleteFollower(otherUserId) {
+    const following = AppState.following
+    const foundFollowing = following.find(f => f.followingId === otherUserId)
+    const res = await api.delete(`api/profiles/${otherUserId}/unfollow/${foundFollowing.id}`)
+    logger.log('deleteFollower', res)
+    AppState.following = AppState.following.filter(f => f.id !== foundFollowing.id)
+  }
 }
 
 export const followService = new FollowService()
