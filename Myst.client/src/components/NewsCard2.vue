121b9b01d7462ebb4ec63ffa2a82c45adff49e86
Derek Shain <derek0anthony@gmail.com>
2021-11-03 14:45:31 -0600

    shadow

--- Myst.client/src/components/NewsCard2.vue
+++ Myst.client/src/components/NewsCard2.vue
@@ -50,12 +50,12 @@ export default {
 <style lang="scss" scoped>
 .newsCard {
 
-  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.404);
   transition: 0.3s;
 }
 
 .newsCard:hover {
-  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.829);
 }
 
 </style>
