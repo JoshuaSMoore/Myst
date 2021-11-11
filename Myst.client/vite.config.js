793fd0e5acefcf8ebff873801ec5b4a947b4d68c
JoshuaSMoore <J_Moore23@hotmail.com>
2021-11-11 10:48:01 -0700

    run build

--- Myst.client/vite.config.js
+++ Myst.client/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: '../myst/myst.client',
+    outDir: '../Myst/client',
     sourcemap: false
   },
   server: {
